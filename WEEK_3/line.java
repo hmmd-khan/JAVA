5038a1f09f52c8159983f4359c9376fae43340e7
hmmd-khan <hammadsaddiqkhan@gmail.com>
2026-08-12 00:29:09 +0500

    Update line.java

--- WEEK_3/line.java
+++ WEEK_3/line.java
@@ -1,21 +1,49 @@
 public class line {
     private Point start;
     private Point end;
+    private static  int objectCount;
 
     line(){
         this.start= new Point();
         this.end=new Point();
+        this.objectCount++;
     }
 
     line(Point start,Point end){
         this.start=start;
         this.end=end;
+        this.objectCount++;
     }
 
     line(line l){
         this.start=l.start;
         this.end=l.end;
+        this.objectCount++;
     }
 
+    Point getStart(){
+        return this.start;
+    }
+
+    Point getEnd(){
+        return this.end;
+    }
     
+    void setStartEnd(Point start,Point end){
+        this.start=start;
+        this.end=end;
+    }
+
+    int getObjectCount(){
+        return this.objectCount;
+    }
+ 
+    @Override
+    public String toString(){
+        return "start point: "+this.start+" end point :"+this.end+" object count: "+line.objectCount;
+    }
+
+    void show(){
+        System.out.println(this.toString());
+    }
 }
